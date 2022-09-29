3ece46e4d003c4d73eb4b0ddae8c1f48e6b23c20
Dmytro Voronievskyi <vdrabota@icloud.com>
2022-09-29 15:20:36 +0200

    search id

--- src/js/api-storage.js
+++ src/js/api-storage.js
@@ -1,3 +1,23 @@
 import { saveToStorage, loadFromStorage } from './storage';
-console.log(saveToStorage);
-console.log(loadFromStorage);
+import { refs } from './refs';
+
+refs.movieGallery.addEventListener('click', onGalleryClick);
+
+let idOfMovie = '';
+
+function onGalleryClick(e) {
+  const swatchEl = e.target;
+  if (!swatchEl.classList.contains('movie-img')) {
+    return;
+  }
+
+  const parentEl = swatchEl.closest('.movie-card');
+
+  console.log(parentEl.dataset.id);
+
+  console.log(typeof parentEl.dataset.id);
+
+  idOfMovie += parentEl.dataset.id;
+}
+
+console.log(idOfMovie);
