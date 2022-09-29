0fea314fd8b53fe48d4f4c1f3f581782314a4183
ViktoriyaZinyak <viktoriazinyak@gmail.com>
2022-09-29 13:21:22 +0300

    js

--- src/js/refs.js
+++ src/js/refs.js
@@ -6,6 +6,9 @@ export const refs = {
   backdropSignUp: document.querySelector('[data-modal-signup]'),
   signUpCloseBtn: document.querySelector('[data-modal-signup-close]'),
   signInLink: document.querySelector('[data-modal-link-signin]'),
+  inputPassword: document.querySelector('[data-password]'),
+  inputPasswordRepeat: document.querySelector('[data-password-repeat]'),
+  formSignUp: document.querySelector('[data-signup]'),
   // preLoader
   // header
   // search
--- src/js/registration/registration.js
+++ src/js/registration/registration.js
@@ -8,11 +8,13 @@ const {
   backdropSignUp,
   signUpCloseBtn,
   signInLink,
+  formSignUp,
 } = refs;
 
 const onSignInBtn = () => {
   backdropSignIn.classList.toggle('is-hidden');
 };
+
 const onModalClose = () => {
   backdropSignIn.classList.toggle('is-hidden');
 };
@@ -29,8 +31,29 @@ const onSignInLink = () => {
   backdropSignIn.classList.toggle('is-hidden');
 };
 
+const handleSubmit = event => {
+  event.preventDefault();
+  console.log(event);
+};
+
 signInBtn.addEventListener('click', onSignInBtn);
 signInCloseBtn.addEventListener('click', onModalClose);
 signUpLink.addEventListener('click', onSignUp);
 signUpCloseBtn.addEventListener('click', onSignUpClose);
 signInLink.addEventListener('click', onSignInLink);
+formSignUp.addEventListener('submit', handleSubmit);
+
+// function handleSubmit(event) {
+//   event.preventDefault();
+//   const {
+//     elements: { login, password },
+//   } = event.currentTarget;
+
+//   if (login.value === '' || password.value === '') {
+//     return console.log('Please fill in all the fields!');
+//   }
+
+//   console.log(`Login: ${login.value}, Password: ${password.value}`);
+//   event.currentTarget.reset();
+// }
+console.log('bjbhkjbhkj');
