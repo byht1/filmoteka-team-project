1ce60db50cbc3c1886055d848ebcec1dc55443b1
Dmytro Voronievskyi <vdrabota@icloud.com>
2022-09-29 16:47:46 +0200

    add event on btn

--- src/js/api-storage.js
+++ src/js/api-storage.js
@@ -1,10 +1,14 @@
 import { saveToStorage, loadFromStorage } from './storage';
 import { refs } from './refs';
 
+const SAVED_WATCHED_MOVIE = 'saved-to-storage-watched-movies';
+
 let idOfMovie = '';
 
 refs.movieGallery.addEventListener('click', onGalleryClick);
+refs.addWatchedBtn.addEventListener('click', onAddWatchedBtnClick);
 
+console.log();
 function onGalleryClick(e) {
   const swatchEl = e.target;
   if (!swatchEl.classList.contains('movie-img')) {
@@ -15,4 +19,17 @@ function onGalleryClick(e) {
 
   idOfMovie = parentEl.dataset.id;
   console.log(idOfMovie);
+}
+
+function onAddWatchedBtnClick(e) {
+  let arrayOfStorage = loadFromStorage(SAVED_WATCHED_MOVIE);
+
+  if (!arrayOfStorage) {
+    arrayOfStorage = [];
+  }
+
+  arrayOfStorage.push(idOfMovie);
+  saveToStorage(SAVED_WATCHED_MOVIE, arrayOfStorage);
+
+  console.log(arrayOfStorage);
 }
--- src/js/refs.js
+++ src/js/refs.js
@@ -1,24 +1,25 @@
 export const refs = {
-    // preLoader
-    // header
-    navLogo: document.querySelector('.navigation__logo'),
-    navPage: document.querySelector('.navigation__page'),
-    searchWrap: document.querySelector('.search-wrap'),
-    libWrap: document.querySelector('.library-button__container'),
-    navPageHome: document.querySelector('.navigation__page-home'),
-    navPageLib: document.querySelector('.navigation__page-library'),
-    header: document.querySelector('header'),
-logoText: document.querySelector('.logo-name'),
-    // search
-    // myLibrary
-    // interface
-    // genere
-    // movie
-    movieGallery: document.querySelector('.movie-gallery'),
-    // footer
-    goItStud: document.querySelector('.goItStud-js'),
-    // modalMovie
-    closeModalBtn: document.querySelector('.modal-close-btn'),
-    modalBackdrop: document.querySelector('.backdrop'),
-    // modalTeam
+  // preLoader
+  // header
+  navLogo: document.querySelector('.navigation__logo'),
+  navPage: document.querySelector('.navigation__page'),
+  searchWrap: document.querySelector('.search-wrap'),
+  libWrap: document.querySelector('.library-button__container'),
+  navPageHome: document.querySelector('.navigation__page-home'),
+  navPageLib: document.querySelector('.navigation__page-library'),
+  header: document.querySelector('header'),
+  logoText: document.querySelector('.logo-name'),
+  // search
+  // myLibrary
+  // interface
+  // genere
+  // movie
+  movieGallery: document.querySelector('.movie-gallery'),
+  // footer
+  goItStud: document.querySelector('.goItStud-js'),
+  // modalMovie
+  closeModalBtn: document.querySelector('.modal-close-btn'),
+  modalBackdrop: document.querySelector('.backdrop'),
+  addWatchedBtn: document.querySelector('[data-action="addWatchedById"]'),
+  // modalTeam
 };
