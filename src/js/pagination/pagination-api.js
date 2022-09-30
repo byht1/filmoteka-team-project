1bd6e0ac58663dfc90973ce7e8473ca384bc3432
byht1 <vitalikbyht1@gmail.com>
2022-09-30 18:50:07 +0300

    Update pagination-api.js

--- src/js/pagination/pagination-api.js
+++ src/js/pagination/pagination-api.js
@@ -1,29 +1,32 @@
-import { refs } from "../refs";
-import Pagination from "./pagination";
-import { renderFilmGallery } from "../movies";
-import { dataMovieList } from '../API/api'
+import { refs } from '../refs';
+import Pagination from './pagination';
+import { renderFilmGallery } from '../movies';
+import { dataMovieList } from '../API/api';
 
 export default function createPagination(data) {
-    const totalPages = data.total_pages;
-    const pagination = new Pagination(document.querySelector("[data-pagination]"), {
-    curr: 1,
-    //   slots: 11,
-    slots: 9,
-    total: totalPages,
-        onChange: page => {
-            getListPerPage(page)
-        }
-    })
-        pagination.init();
+  const totalPages = data.total_pages;
+  const pagination = new Pagination(
+    document.querySelector('[data-pagination]'),
+    {
+      curr: 1,
+      //   slots: 11,
+      slots: 9,
+      total: totalPages,
+      onChange: page => {
+        getListPerPage(page);
+      },
+    }
+  );
+  pagination.init();
 }
 
 async function getListPerPage(page) {
-    // console.log("Yeah, its works, we on:", page);
-    clearFilmsContainer();
-    const data = await dataMovieList(page, language = 'en-US');
-    renderFilmGallery(data);
+  // console.log("Yeah, its works, we on:", page);
+  clearFilmsContainer();
+  const data = await dataMovieList(page);
+  renderFilmGallery(data);
 }
 
 function clearFilmsContainer() {
-    refs.movieGallery.innerHTML = "";
+  refs.movieGallery.innerHTML = '';
 }
