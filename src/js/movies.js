91b27982d3e90cc71c8ec4c2f8194441a534f9a6
byht1 <vitalikbyht1@gmail.com>
2022-10-01 18:14:26 +0300

    scroll

--- src/js/movies.js
+++ src/js/movies.js
@@ -45,7 +45,10 @@ export async function renderFilmGallery(movies) {
     })
     .join('');
 
+  refs.movieGallery.classList.add('movie-height');
+
   refs.movieGallery.insertAdjacentHTML('afterbegin', markup);
 
   window.scrollTo(0, 0);
+  refs.movieGallery.classList.remove('movie-height');
 }
