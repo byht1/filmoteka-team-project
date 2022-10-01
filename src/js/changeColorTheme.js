a7bc2a81b25616f3ab5f324d11249276c44c27bd
Grek Maria <grekmariaa@gmail.com>
2022-10-01 21:58:49 +0300

    fix change color theme
    обрана тема зберігається після перезавантаження сторінки

--- src/js/changeColorTheme.js
+++ src/js/changeColorTheme.js
@@ -3,5 +3,25 @@ import { refs } from './refs';
 refs.toggleTheme.addEventListener('input', changeTheme);
 
 function changeTheme(evt) {
-    evt.target.checked ? refs.html.setAttribute('data-mode', 'dark') : refs.html.setAttribute('data-mode', 'light');
+    if (evt.target.checked) {
+        refs.html.setAttribute('data-mode', 'dark');
+        localStorage.setItem('data-mode', 'dark');
+    } else {
+        refs.html.setAttribute('data-mode', 'light');
+        localStorage.setItem('data-mode', 'light');
+    }
 }
+
+// localStorage.removeItem('data-mode');
+// console.log(localStorage.getItem('data-mode'));
+
+function chechedLocalStorageTheme() {
+    if (localStorage.getItem('data-mode') == null) {
+        return;
+    } else {
+        let dataMode = localStorage.getItem('data-mode');
+        refs.html.setAttribute('data-mode', `${dataMode}`);
+    }
+}
+
+chechedLocalStorageTheme();
