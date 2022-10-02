2cee8acad4c2a8ba6bf30cb8d81851eb04612990
Nazarii7 <nazariipushkaruk@gmail.com>
2022-10-02 23:11:20 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,4 +3,8 @@ import './js/header';
 import './js/movies';
 import './js/modalTeam';
 import './js/login';
+<<<<<<< Updated upstream
 import './js/preLoader'
+=======
+import './js/preLoader';
+>>>>>>> Stashed changes
