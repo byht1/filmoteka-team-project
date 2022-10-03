c97592d5553942160e4948b3e1e1fa10583800f5
Grek Maria <grekmariaa@gmail.com>
2022-10-03 23:57:17 +0300

    Update genre.js

--- src/js/translation/genre.js
+++ src/js/translation/genre.js
@@ -1,11 +1,9 @@
 import { refs } from '../refs';
 
-const genreText = genreTextdocument.querySelector('.show-burron-general');
-console.log('~ genreText', genreText);
 export function changeLanguageGenre() {
     if (refs.html.getAttribute('lang') == 'uk') {
-        genreText.textContent = 'Жанри';
+        refs.genreText.textContent = 'Жанри';
     } else {
-        genreText.textContent = 'Genres';
+        refs.genreText.textContent = 'Genres';
     }
 }
