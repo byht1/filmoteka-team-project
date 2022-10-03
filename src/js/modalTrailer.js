d9168093093c81698a5374634026ad4747590cc5
byht1 <vitalikbyht1@gmail.com>
2022-10-03 21:54:02 +0300

    actor fix

--- src/js/modalTrailer.js
+++ src/js/modalTrailer.js
@@ -2,7 +2,6 @@ import { dataTrailer } from './API/api';
 
 export async function renderVideo(id) {
   const { results } = await dataTrailer(id);
-  console.log('🚀 ~ results', results);
 
   const render = `<iframe width="560" height="315" src="https://www.youtube.com/embed/${results[0].key}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
   const playBox = document.querySelector('#player');
